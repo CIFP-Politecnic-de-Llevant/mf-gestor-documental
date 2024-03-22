79e9fc2ac6b450091433fab951538d1dba443674
Joan Galmés <joan.galmes@gmail.com>
2024-03-22 13:05:34 +0100

    sticky header

--- src/pages/FCTTeacherManager.vue
+++ src/pages/FCTTeacherManager.vue
@@ -107,6 +107,7 @@
         row-key="name"
         binary-state-sort
         :pagination="initialPagination"
+        class="sticky-header-table"
       >
         <template v-slot:header="props">
           <q-tr :props="props">
@@ -506,5 +507,38 @@ onMounted(async ()=>{
 .text-wrap{
   text-wrap: wrap;
   text-align: left;
+}
+
+.sticky-header-table {
+  /* height or max-height is important */
+  height: calc(100vh - 200px);
+
+  . q-table__top,
+  . q-table__bottom,
+  thead tr:first-child th {
+    /* bg color is important for th; just specify one */
+    background-color: #00b4ff;
+  }
+
+  thead tr th {
+    position: sticky;
+    z-index: 1;
+  }
+
+  thead tr:first-child th {
+    top: 0
+  }
+
+  /* this is when the loading indicator appears */
+  &.q-table--loading thead tr:last-child th {
+    /* height of all previous header rows */
+    top: 48px;
+  }
+
+  /* prevent scrolling behind sticky top row on focus */
+  tbody {
+    /* height of all previous header rows */
+    scroll-margin-top: 48px;
+  }
 }
 </style>
